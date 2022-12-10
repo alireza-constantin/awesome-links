46f750386a582d616c7ea6232787306cb4639aa8
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-12-10 16:16:42 +0330

    init project

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const about = () => {
-  return (
-    <div>
-      <h1>Hello from about</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello from about</h1>
+		</div>
+	);
 };
 
 export default about;
