89cb67f7f2ae7f3eb8cdc6af8a873635c5515227
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-12-25 23:20:02 +0330

    get links from client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/tailwind.css';
 import Layout from '../components/Layout';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../lib/apollo';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<ApolloProvider client={client}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ApolloProvider>
+	);
 }
 
 export default MyApp;
